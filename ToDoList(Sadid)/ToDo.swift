2489da893d442b3a1e882337eddf540c78bad352
Md. Sadidur Rahman <mdsadidurrahman74@gmail.com>
2018-05-15 14:15:27 +0600

    Full app completed

---
+++ ToDoList(Sadid)/ToDo.swift
@@ -0,0 +1,9 @@
+//
+//  ToDo.swift
+//  ToDoList(Sadid)
+//
+//  Created by BinaryVentures_Sadid on 5/14/18.
+//  Copyright © 2018 BinaryVentures_Sadid. All rights reserved.
+//
+
+import Foundation
---
+++ ToDoList(Sadid)/ToDoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ToDoTableViewCell.swift
+//  ToDoList(Sadid)
+//
+//  Created by BinaryVentures_Sadid on 5/14/18.
+//  Copyright © 2018 BinaryVentures_Sadid. All rights reserved.
+//
+
+import UIKit
+
+class ToDoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
